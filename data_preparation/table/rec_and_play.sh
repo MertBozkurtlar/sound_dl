3b67b6735f7962ce1efaef67870da7d9e0315a5d
Mert Bozkurtlar <mert.bozkurtlar@outlook.com>
2023-10-19 15:36:33 +0300

    Folder organization

--- data_preparation/table/rec_and_play.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# select the recording device from arecord -l
-arecord -d 17 -r 16000 -c 60 -f S24_3LE -D hw:2,0 a.wav &
-
-# need D option if you change the device, see aplay -L
-aplay 16384.little_endian_16times_tsp.wav
